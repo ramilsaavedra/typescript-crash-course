4d420dcd4264bc75bac3da9c9f91ae5d4b95b38f
Ramil Saavedra <ramilsmcl@gmail.com>
2022-05-18 04:48:56 +0800

    .

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,79 @@
 "use strict";
-let id = 1;
-id = 2;
-console.log(id, 'id');
+// Basic Types
+let id = 5;
+let company = 'Traversy Media';
+let isPublished = true;
+let x = 'Hi';
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, 'hello', true];
+// Tuple
+let person = [1, 'Ramil', true];
+// Tuple Array
+let people = [
+    [1, 'Jet'],
+    [2, 'Ramil'],
+];
+// Union
+let _id;
+_id = 2;
+_id = '2';
+// Enum
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 0] = "Up";
+    Direction1[Direction1["Down"] = 1] = "Down";
+    Direction1[Direction1["Left"] = 2] = "Left";
+    Direction1[Direction1["Right"] = 3] = "Right";
+})(Direction1 || (Direction1 = {}));
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+const user = {
+    id: 1,
+    name: 'John',
+};
+// Type Assertion
+let cid = 1;
+// let customerId = <number>cid;
+let customerId = cid;
+// Functions
+function AddNum(x, y) {
+    return x + y;
+}
+function log(message) {
+    console.log(message);
+}
+const user1 = {
+    id: 1,
+    name: 'John',
+};
+const add = (x, y) => {
+    return x + y;
+};
+const sub = (x, y) => {
+    return x - y;
+};
+class Person {
+    constructor(id, age, name) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+    }
+    register() {
+        return `${this.name} is now registered`;
+    }
+}
+class Employee extends Person {
+    constructor(id, age, name, position) {
+        super(id, age, name);
+        this.position = position;
+    }
+}
+// Generics
+function getArray(items) {
+    return new Array().concat(items);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,143 @@
-let id = 1;
+// Basic Types
+let id: number = 5;
+let company: string = 'Traversy Media';
+let isPublished: boolean = true;
+let x: any = 'Hi';
+let ids: number[] = [1, 2, 3, 4, 5];
+let arr: any[] = [1, 'hello', true];
 
-id = 2;
+// Tuple
+let person: [number, string, boolean] = [1, 'Ramil', true];
+// Tuple Array
+let people: [number, string][] = [
+  [1, 'Jet'],
+  [2, 'Ramil'],
+];
 
-console.log(id, 'id');
+// Union
+let _id: number | string;
+
+_id = 2;
+_id = '2';
+
+// Enum
+enum Direction1 {
+  Up,
+  Down,
+  Left,
+  Right,
+}
+
+enum Direction2 {
+  Up = 'Up',
+  Down = 'Down',
+  Left = 'Left',
+  Right = 'Right',
+}
+
+// Objects
+type User = {
+  id: number;
+  name: string;
+};
+
+const user: User = {
+  id: 1,
+  name: 'John',
+};
+
+// Type Assertion
+let cid: any = 1;
+// let customerId = <number>cid;
+let customerId = cid as number;
+
+// Functions
+function AddNum(x: number, y: number): number {
+  return x + y;
+}
+
+function log(message: string | number): void {
+  console.log(message);
+}
+
+// Interfaces
+interface UserInterface {
+  readonly id: number;
+  name: string;
+  //optional
+  age?: number;
+}
+
+const user1: UserInterface = {
+  id: 1,
+  name: 'John',
+};
+
+interface MathFunc {
+  (x: number, y: number): number;
+}
+
+const add: MathFunc = (x, y) => {
+  return x + y;
+};
+
+const sub: MathFunc = (x, y) => {
+  return x - y;
+};
+
+// Classes
+
+// class Person {
+//   // accessible only within the class
+//   private id: number;
+//   // accessible only within the class and its subclasses
+//   protected age: number;
+//   name: string;
+
+//   constructor(id: number, age: number, name: string) {
+//     this.id = id;
+//     this.age = age;
+//     this.name = name;
+//   }
+
+//   register() {
+//     return `${this.name} is now registered`;
+//   }
+// }
+
+interface PersonInterface {
+  id: number;
+  name: string;
+  age: number;
+  register(): string;
+}
+
+class Person implements PersonInterface {
+  id: number;
+  age: number;
+  name: string;
+
+  constructor(id: number, age: number, name: string) {
+    this.id = id;
+    this.age = age;
+    this.name = name;
+  }
+
+  register() {
+    return `${this.name} is now registered`;
+  }
+}
+
+class Employee extends Person {
+  position: string;
+
+  constructor(id: number, age: number, name: string, position: string) {
+    super(id, age, name);
+    this.position = position;
+  }
+}
+
+// Generics
+function getArray<T>(items: T[]): T[] {
+  return new Array().concat(items);
+}
